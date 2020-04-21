4c10a06e3fd0a938ff89ef538b46ae6a793aeae5
Edwin Belduma <edwineverth94@gmail.com>
2020-04-21 18:50:40 -0500

    Conocimiento de ReactDOM.Render

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-// const element = document.createElement('h1');
-// element.innerText = 'Hello, Platzi Badges!';
+// const element = document.createElement("h1");
+// element.innerText = "Hola este es mi proyecto!";
 
-// const container = document.getElementById('app');
+// const container = document.getElementById("app");
 
 // container.appendChild(element);
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const element = <h1>Hello, Platzi Badges!</h1>;
+const element = < h1 > Hola este es mi proyecto en react < /h1>;
 
-const container = document.getElementById('app');
+const container = document.getElementById("app");
 
-// ReactDOM.render(__qué__, __dónde__);
+// ReactDOM.render(_que__, __donde__) // Dos argumentos, 1. que queremos renderizar, y donde vamos a renderizar
+
 ReactDOM.render(element, container);
