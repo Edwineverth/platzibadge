a94874b65e4fd782256f7dd3babb081aac1fa157
Edwin Belduma <edwineverth94@gmail.com>
2020-04-21 19:23:02 -0500

    2. JSX y React Create Element

--- src/index.js
+++ src/index.js
@@ -8,8 +8,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const element = < h1 > Hola este es mi proyecto en react < /h1>;
+//const jsx = < h1 > Hola este es mi proyecto en react < /h1>;
+// const element = React.createElement("h1", {}, "Hola soy los children");
+// const link = React.createElement(
+//     "a", { href: "https://www.facebook.com" },
+//     "Ir a facebook"
+// );
+const name = "Edwin";
+//const element = React.createElement("h1", {}, `Hola soy ${name}`);
 
+// const sum = () => 3 + 3;
+// const jsx = < h1 > Hola soy,
+//     {sum()} < /h1>;
+
+const jsx = (
+  <div>
+    <h1>Hola soy {name}</h1>
+    <p>Soy Ingeniero de Sistemas</p>
+  </div>
+);
+
+const element = React.createElement(
+  "div",
+  {},
+  React.createElement("h1", {}, "Hola soy Edwin"),
+  React.createElement("p", {}, "Soy Ingeniero de Sistemas")
+);
 const container = document.getElementById("app");
 
 // ReactDOM.render(_que__, __donde__) // Dos argumentos, 1. que queremos renderizar, y donde vamos a renderizar
