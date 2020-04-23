c796f68eaa0d596d13fc336281a1183f86715c34
Edwin Belduma <edwineverth94@gmail.com>
2020-04-23 17:55:17 -0500

    Creando una pagina con componentes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Badge from "./components/Badge";
+import BadgeNew from "./pages/BadgeNew.js";
 import "bootstrap/dist/css/bootstrap.css";
 import "./global.css";
 const container = document.getElementById("app");
-ReactDOM.render(
-  <Badge
-    firstName="Dennys"
-    lastName="Belduma"
-    jobTitle="Developer"
-    avatar="https://www.gravatar.com/avatar?d=identicon"
-    twtter="edwineverth94"
-  />,
-  container
-);
+ReactDOM.render(<BadgeNew />, container);
---
+++ src/pages/BadgeNew.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import header from "../images/badge-header.svg";
+import "./styles/BadgeNew.css";
+import Badge from "../components/Badge";
+class BadgeNew extends React.Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div className="BadgeNew__hero">
+          <img className="img-fluid" src={header} alt="logo" />
+        </div>
+
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <Badge
+                firstName="Edwin"
+                lastName="Belduma"
+                twtter="edwineverth94"
+                jobTitle="Developer"
+                avatar="https://s.gravatar.com/avatar/11efc8ed929adc88aab8cb0100a6a074?s=80
+"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default BadgeNew;
