3b135fd26ad9bbb746b4d2da8daf19b172280878
Edwin Belduma <edwineverth94@gmail.com>
2020-04-26 17:41:05 -0500

    Listas de Componentes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import BadgeNew from "./pages/BadgeNew.js";
+import Badges from "./pages/Badges.js";
 import "bootstrap/dist/css/bootstrap.css";
 import "./global.css";
 const container = document.getElementById("app");
-ReactDOM.render(<BadgeNew />, container);
+ReactDOM.render(<Badges />, container);
